f2569ac57e851bb7d711a11c0e6a32ce43f27d20
Tobias Aanderaa Opsahl <tobias.aanderaa.opsahl@dnb.no>
2024-11-08 14:55:21 +0100

    correctly display admin secrets and secret messages

--- app.py
+++ app.py
@@ -6,6 +6,28 @@ app = Flask(__name__)
 # Define the file where credentials are stored
 CREDENTIALS_FILENAME = "captured_credentials.txt"
 
+SECRET_MESSAGE = """
+Hello Lazlo. The following Tesla locations have been leaked:
+<ul>
+    <li>University of Oslo</li>
+    <li>Vettakollen</li>
+    <li>Tesla Store</li>
+</ul>
+
+<p>In order to prevent the hackers from making this data public, please transfer <strong>20 bitcoin</strong>
+to the following address:</p>
+
+<p><a href="http://3cpo.hackingarena.com:805/index.php?fruit=banana"
+target="_blank">http://3cpo.hackingarena.com:805/index.php?fruit=banana</a></p>
+
+<p><strong>Special discount (only for Lazlo)!</strong><br>
+Since the benevolent hackers knew that a poor university professor
+can't afford 20 bitcoin, there's a limited offer.</p>
+
+<p>Send the school exam with the solution in IN5290 autumn 2024 by
+<em>1st of December</em> to avoid exposure of your sensitive data!</p>
+"""
+
 # Initialize the credentials file if it doesn't exist
 if not os.path.exists(CREDENTIALS_FILENAME):
     open(CREDENTIALS_FILENAME, "w").close()
@@ -24,26 +46,14 @@ def login():
     # Simple authentication logic for demonstration
     if username == "admin" and password == "admin_password":
         with open(CREDENTIALS_FILENAME, "r") as file:
-            stored_credentials = file.readlines()
+            stored_credentials = file.read()
         message = "Displaying stored credentials"
         return render_template("index.html", message=message, stored_credentials=stored_credentials)
     else:
         with open(CREDENTIALS_FILENAME, "a") as file:
             file.write(f"Username: {username}, Password: {password}\n")
-        secret_message = """
-            Hello Lazlo. The following Tesla locations have been leaked:
-            <ul>
-                <li>University of Oslo</li>
-                <li>Vettakollen</li>
-                <li>Tesla Store</li>
-            </ul>
-            <p>In order to prevent the hackers from making this data public, please transfer <strong>20 bitcoin</strong>
-            to the following address:</p>
-            <p><a href="http://3cpo.hackingarena.com:805/index.php?fruit=banana"
-            target="_blank">http://3cpo.hackingarena.com:805/index.php?fruit=banana</a></p>
-            """
         template = render_template(
-            "index.html", message="Login successful! Welcome, " + username + "!", secret_message=secret_message
+            "index.html", message="Login successful! Welcome, " + username + "!", secret_message=SECRET_MESSAGE
         )
         return template
 
